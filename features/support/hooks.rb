c3c6f1d75c39e8486b6792f7a764ca68d2e6d374
Jacques de Jesus Figueiredo Schmitz Junior <jacques.schmitz@compasso.com.br>
2021-03-12 20:09:33 -0300

    refactor: pasta de logs

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,7 +6,7 @@ Before do
 end
 
 AfterStep do
-  screenshot = "log/screenshot#{Time.now.to_i.to_s}.png"
+  screenshot = "reports/screenshot#{Time.now.to_i.to_s}.png"
   page.save_screenshot(screenshot)
   embed(screenshot, 'image/png', 'the_screenshot')
 end
